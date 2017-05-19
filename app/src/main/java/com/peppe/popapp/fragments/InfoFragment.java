8c3c1fdd14bece2a73b3e2076a01c03177d7d19c
giuseppe.spallone95@gmail.com <peppespallone95>
2017-05-19 20:21:40 +0200

    Aggiunta chiamata ed email a infoFragment, ma non funziona

--- app/src/main/java/com/peppe/popapp/fragments/InfoFragment.java
+++ app/src/main/java/com/peppe/popapp/fragments/InfoFragment.java
@@ -1,6 +1,7 @@
 package com.peppe.popapp.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,19 +9,44 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.peppe.popapp.R;
 
 public class InfoFragment extends Fragment {
 
-    public InfoFragment() {
-    }
+    TextView textViewTelefono, textViewEmail;
+    String numeroTelefono, email;
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
         getActivity().setTitle("Info");
+
+        textViewTelefono = (TextView) getView().findViewById(R.id.textViewTelefono);
+        textViewEmail = (TextView) getView().findViewById(R.id.textViewEmail);
+
+        numeroTelefono = textViewTelefono.getText().toString();
+        email = textViewEmail.getText().toString();
+
+        textViewTelefono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent chiamata = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + numeroTelefono));
+                startActivity(chiamata);
+            }
+        });
+
+
+        textViewEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:fvdvd"));
+                startActivity(email);
+            }
+        });
     }
 
     @Nullable
